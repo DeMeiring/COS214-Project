b94c5107fee27a98a29ae49fd8091504043f16a5
Joseph <jh.harraway@gmail.com>
2020-11-09 19:35:03 +0200

    Completed Implementation

--- BluePrint.cpp
+++ BluePrint.cpp
@@ -1,11 +1,9 @@
 #include "BluePrint.h"
 
 Engineering* BluePrint::getCarBluePrint() {
-	// TODO - implement BluePrint::getCarBluePrint
 	return CarBluePrint;
 }
 
 void BluePrint::setCarBluePrint(Engineering* CarBluePrint) {
-	// TODO - implement BluePrint::setCarBluePrint
 	this->CarBluePrint = CarBluePrint;
 }
